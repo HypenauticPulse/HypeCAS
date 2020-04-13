628078f8569d04ee9ca6b2a9d61f079ba125c056
HypenauticPulse <ctvnunes@gmail.com>
2020-04-13 12:30:44 -0500

    Redesigned the polynomial interpreter.
       - Separated first by spaces instead of by the variable.
       - Move the interpreter to its own file as a function.

--- polyconvert.py
+++ polyconvert.py
@@ -1,1 +1,52 @@
-def poly_conversion()
+def poly_conversion(eq, var):
+    poly = eq.split()
+
+    print(poly)
+    coeffPower = []
+    i = 1
+
+    while i < len(poly):
+        print("i =", i)
+        poly.insert(i, poly[i] + poly[i + 1])
+        poly.pop(i + 1)
+        poly.pop(i + 1)
+        print(poly)
+        i += 1
+
+    for j in poly:
+        cp = j.split(var)
+        print(cp)
+        if len(cp) == 1:
+            cp.append(0)
+            for k in range(len(cp)):
+                cp[k] = float(cp[k])
+            coeffPower.append(cp)
+        else:
+            if '^' in cp[1]:
+                if cp[0] == '':
+                    Temp = cp[1]
+                    Temp.split('^')
+                    cp.pop(1)
+                    cp.append(Temp[1])
+                    cp.pop(0)
+                    cp.insert(0, 1)
+                    for k in range(len(cp)):
+                        cp[k] = float(cp[k])
+                    coeffPower.append(cp)
+                else:
+                    Temp = cp[1]
+                    Temp.split('^')
+                    cp.pop(1)
+                    cp.append(Temp[1])
+                    # print("cp alpha:", cp)
+                    for k in range(len(cp)):
+                        cp[k] = float(cp[k])
+                    coeffPower.append(cp)
+            elif cp[1] == '':
+                cp.pop(1)
+                cp.append(1)
+                for k in range(len(cp)):
+                    cp[k] = float(cp[k])
+                coeffPower.append(cp)
+
+    return coeffPower
