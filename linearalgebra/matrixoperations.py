b71eac142d0fcc08d8ff17e3a8cbfea9cab296e3
HypenauticPulse <ctvnunes@gmail.com>
2020-06-03 00:16:10 -0500

    More Matrix Operations.
    - Implemented a row echelon form algorithm (copied from the first half of the reduced form algorithm).
    - Added matrix addition, subtraction, scalar multiplication, and matrix multiplication.
    - Added the ability to create minors from a matrix.
    - Implemented an algorithm to find the upper portion of the LU decomposition of a matrix.
    - Implemented determinants using the upper triangular matrix from LU decomposition.

--- linearalgebra/matrixoperations.py
+++ linearalgebra/matrixoperations.py
@@ -1,15 +1,15 @@
-def swap_rows(row_a, row_b, aug_mat):
+def ero_swap_rows(row_a, row_b, aug_mat):
     temp = aug_mat[row_b]
     aug_mat[row_b] = aug_mat[row_a]
     aug_mat[row_a] = temp
 
 
-def multiply_scalar(row, scalar, aug_mat):
+def ero_multiply_scalar(row, scalar, aug_mat):
     for i in range(len(aug_mat[row])):
         aug_mat[row][i] *= scalar
 
 
-def multiply_add_row(row_a, row_b, scalar, aug_mat):
+def ero_multiply_add_row(row_a, row_b, scalar, aug_mat):
     for i in range(len(aug_mat[row_a])):
         aug_mat[row_b][i] += scalar * aug_mat[row_a][i]
 
@@ -19,17 +19,7 @@ def rref(aug_mat):
     for i in aug_mat:
         temp.append(i)
 
-    for i in range(len(temp)):
-        if temp[i][i] == 0:
-            trigger = 0
-            j = i + 1
-            while j < len(temp) and not trigger:
-                if temp[j][i] != 0:
-                    trigger = 1
-                else:
-                    j += 1
-            if trigger:
-                swap_rows(i, j, temp)
+    temp = swap_zero_pivots(temp)
 
     for i in range(len(temp)):
         j = i
@@ -42,10 +32,10 @@ def rref(aug_mat):
             j += 1
         if pivot != -1:
             if temp[i][pivot] != 1 and temp[i][pivot] != 0:
-                multiply_scalar(i, 1.0 / temp[i][pivot], temp)
+                ero_multiply_scalar(i, 1.0 / temp[i][pivot], temp)
             for j in range(i + 1, len(temp)):
                 if temp[j][pivot] != 0:
-                    multiply_add_row(i, j, - temp[j][pivot], temp)
+                    ero_multiply_add_row(i, j, - temp[j][pivot], temp)
 
     for i in reversed(range(len(temp))):
         j = i
@@ -58,11 +48,42 @@ def rref(aug_mat):
             j += 1
         if pivot != -1:
             if temp[i][pivot] != 1 and temp[i][pivot] != 0:
-                multiply_scalar(i, 1.0 / temp[i][pivot], temp)
+                ero_multiply_scalar(i, 1.0 / temp[i][pivot], temp)
             if temp[i][pivot] == 1:
                 for j in reversed(range(i)):
                     if temp[j][pivot] != 0:
-                        multiply_add_row(i, j, - temp[j][pivot], temp)
+                        ero_multiply_add_row(i, j, - temp[j][pivot], temp)
+
+    for i in range(len(temp)):
+        for j in range(len(temp[i])):
+            if temp[i][j] == -0.0:
+                temp[i][j] = 0.0
+
+    return temp
+
+
+def echelon_form(aug_mat):
+    temp = []
+    for i in aug_mat:
+        temp.append(i)
+
+    temp = swap_zero_pivots(temp)
+
+    for i in range(len(temp)):
+        j = i
+        trigger = 0
+        pivot = -1
+        while j < len(temp[i]) and not trigger:
+            if temp[i][j] != 0.0:
+                trigger = 1
+                pivot = j
+            j += 1
+        if pivot != -1:
+            if temp[i][pivot] != 1 and temp[i][pivot] != 0:
+                ero_multiply_scalar(i, 1.0 / temp[i][pivot], temp)
+            for j in range(i + 1, len(temp)):
+                if temp[j][pivot] != 0:
+                    ero_multiply_add_row(i, j, - temp[j][pivot], temp)
 
     for i in range(len(temp)):
         for j in range(len(temp[i])):
@@ -75,3 +96,138 @@ def rref(aug_mat):
 def print_matrix(matrix):
     print('\n'.join([''.join(['{:7}'.format(round(item, 2)) for item in row])
                      for row in matrix]))
+
+
+def matrix_add(matrix_a, matrix_b):
+    if len(matrix_a) != len(matrix_b) or len(matrix_a[0]) != len(matrix_b[0]):
+        raise ValueError(
+            "Dimension mismatch for addition between {}x{} and {}x{} matrices.".format(len(matrix_a), len(matrix_a[0]), len(matrix_b), len(matrix_b[0])))
+    else:
+        result = []
+        for i in range(len(matrix_a)):
+            row_result = []
+            for j in range(len(matrix_a[0])):
+                row_result.append(matrix_a[i][j] + matrix_b[i][j])
+            result.append(row_result)
+    return result
+
+
+def matrix_sub(matrix_a, matrix_b):
+    if len(matrix_a) != len(matrix_b) or len(matrix_a[0]) != len(matrix_b[0]):
+        raise ValueError(
+            "Dimension mismatch for subtraction between {}x{} and {}x{} matrices.".format(len(matrix_a), len(matrix_a[0]), len(matrix_b), len(matrix_b[0])))
+    else:
+        temp = []
+        for i in range(len(matrix_a)):
+            row_temp = []
+            for j in range(len(matrix_a[0])):
+                row_temp.append(- matrix_b[i][j])
+            temp.append(row_temp)
+    return matrix_add(matrix_a, temp)
+
+
+def matrix_scalar_multiply(matrix, scalar):
+    result = []
+    for i in range(len(matrix)):
+        row_result = []
+        for j in range(len(matrix[0])):
+            row_result.append(scalar * matrix[i][j])
+        result.append(row_result)
+
+    return result
+
+
+def matrix_multiplication(matrix_a, matrix_b):
+    if len(matrix_a) != len(matrix_b[0]) or len(matrix_a[0]) != len(matrix_b):
+        raise ValueError(
+            "Dimension mismatch for multiplication between {}x{} and {}x{} matrices.".format(len(matrix_a), len(matrix_a[0]), len(matrix_b), len(matrix_b[0])))
+    else:
+        result = []
+        for i in range(len(matrix_a)):
+            row_result = []
+            for j in range(len(matrix_b[0])):
+                term_result = 0
+                for k in range(len(matrix_b)):
+                    term_result += matrix_a[i][k] * matrix_b[k][j]
+                row_result.append(term_result)
+            result.append(row_result)
+
+    return result
+
+
+def create_minor(matrix, r_row, r_col):
+    if not r_row <= len(matrix) or not r_col <= len(matrix[0]):
+        raise ValueError("Invalid row, {}, or column, {}, to remove from an {}x{} matrix".format(r_row, r_col, len(matrix), len(matrix[0])))
+    else:
+        temp = []
+        for i in range(len(matrix)):
+            if i != r_row - 1:
+                row_temp = []
+                for j in range(len(matrix[0])):
+                    if j != r_col - 1:
+                        row_temp.append(matrix[i][j])
+                temp.append(row_temp)
+
+    return temp
+
+
+def determinant(matrix):
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("Determinants can only be found for square matrices.")
+    else:
+        temp = lu_decomposition_upper(matrix)
+        det = 1
+        for i in range(len(matrix)):
+            det *= temp[i][i]
+
+    return det
+
+
+def swap_zero_pivots(matrix):
+    temp = []
+    for i in matrix:
+        temp.append(i)
+
+    for i in range(len(temp)):
+        if temp[i][i] == 0:
+            trigger = 0
+            j = 0
+            while j < len(temp) and not trigger:
+                if temp[j][i] != 0:
+                    trigger = 1
+                else:
+                    j += 1
+            if trigger:
+                ero_swap_rows(i, j, temp)
+
+    return temp
+
+
+def lu_decomposition_upper(matrix):
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("Only square matrices can undergo LU decomposition.")
+    else:
+        temp = []
+        for i in matrix:
+            temp.append(i)
+
+        for i in range(len(temp)):
+            j = i
+            trigger = 0
+            pivot = -1
+            while j < len(temp[i]) and not trigger:
+                if temp[i][j] != 0.0:
+                    trigger = 1
+                    pivot = j
+                j += 1
+            if pivot != -1:
+                for j in range(i + 1, len(temp)):
+                    if temp[j][pivot] != 0:
+                        ero_multiply_add_row(i, j, - temp[j][pivot] / temp[i][pivot], temp)
+
+        for i in range(len(temp)):
+            for j in range(len(temp[i])):
+                if temp[i][j] == -0.0:
+                    temp[i][j] = 0.0
+
+    return temp
--- testing.py
+++ testing.py
@@ -3,41 +3,46 @@ from linearalgebra import matrixoperations as matop
 from polynomials.LinearMultivariateEquality import LinearMultivariateEquality as LMvE
 from polyutils import polyoperations as polyop, polyconversion as polycon
 
-eq1 = 'x + 2y + 3z = 0'
-eq2 = '4x + 5y + 6z = 0'
-eq3 = '7x + 8y + 9z = 0'
-var = ['x', 'y', 'z']
+mat_a = [[1, 3, 5], [0, 5, 1], [6, 5, 0]]
+mat_b = [[4, 5], [6, 7]]
 
-test = LinSys(var, eq1, eq2, eq3)
-print(test.lhs)
-print(test.rhs)
-print('\n\n')
-matop.print_matrix(test.augmented_matrix)
-rref_mat = matop.rref(test.augmented_matrix)
-print('\n\n')
-matop.print_matrix(rref_mat)
+print(matop.determinant(mat_b))
 
-valid_count = 0
-for i in rref_mat:
-    valid = 1
-    coeff = i[0:len(var)]
-    constant = i[len(var)]
-    if not any(coeff) and constant != 0:
-        valid = 0
-    valid_count += valid
+# eq1 = 'x + 2y + 3z = 0'
+# eq2 = '4x + 5y + 6z = 0'
+# eq3 = '7x + 8y + 9z = 0'
+# var = ['x', 'y', 'z']
+#
+# test = LinSys(var, eq1, eq2, eq3)
+# print(test.lhs)
+# print(test.rhs)
+# print('\n\n')
+# matop.print_matrix(test.augmented_matrix)
+# rref_mat = matop.echelon_form(test.augmented_matrix)
+# print('\n\n')
+# matop.print_matrix(rref_mat)
 
-if valid_count == len(rref_mat):
-    print("This system of equations has solutions")
-    free_count = 0
-    free_vars = []
-    for i in range(len(rref_mat)):
-        if not any(rref_mat[i]):
-            free_count += 1
-            free_vars.append(i)
-    print(free_count, "variable(s) are free")
-    print('Free variables are:')
-    for idx in free_vars:
-        print(var[idx])
-
-else:
-    print("This system of equations is inconsistent")
+# valid_count = 0
+# for i in rref_mat:
+#     valid = 1
+#     coeff = i[0:len(var)]
+#     constant = i[len(var)]
+#     if not any(coeff) and constant != 0:
+#         valid = 0
+#     valid_count += valid
+#
+# if valid_count == len(rref_mat):
+#     print("This system of equations has solutions")
+#     free_count = 0
+#     free_vars = []
+#     for i in range(len(rref_mat)):
+#         if not any(rref_mat[i]):
+#             free_count += 1
+#             free_vars.append(i)
+#     print(free_count, "variable(s) are free")
+#     print('Free variables are:')
+#     for idx in free_vars:
+#         print(var[idx])
+#
+# else:
+#     print("This system of equations is inconsistent")
